68ec0120f4cd2bbbe2bc2d9341fce6aae8a215ef
Yudai NAKATA <yudai.nkt@gmail.com>
2024-07-28 04:51:43 +0900

    feat: add Vial-enabled keymap for cocot46plus

---
+++ keyboards/aki27/cocot46plus/keymaps/vial/config.h
@@ -0,0 +1,13 @@
+/* SPDX-License-Identifier: GPL-2.0-or-later */
+
+#pragma once
+
+#define VIAL_KEYBOARD_UID {0x4D, 0x5C, 0xCE, 0x1F, 0xB7, 0x02, 0xDC, 0x8F}
+#define VIAL_UNLOCK_COMBO_ROWS { 0, 7 }
+#define VIAL_UNLOCK_COMBO_COLS { 0, 0 }
+
+#define POINTING_DEVICE_AUTO_MOUSE_ENABLE
+#define AUTO_MOUSE_DEFAULT_LAYER 3
+
+#define I2C1_SDA_PIN GP2
+#define I2C1_SCL_PIN GP3
